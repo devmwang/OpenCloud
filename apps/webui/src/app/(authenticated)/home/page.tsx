e12e24f66cc7d5f43f61c927c0918c8c31517e3b
Michael Wang <41971562+devmwang@users.noreply.github.com>
2023-08-20 22:54:30 -0700

    feat: Implement "getServerSession" to get session details on server (ie RSC)

--- apps/webui/src/app/(authenticated)/home/page.tsx
+++ apps/webui/src/app/(authenticated)/home/page.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+import { getServerSession } from "@/components/auth/server-session";
+
+export default async function Home() {
+    const session = await getServerSession();
+
     return (
         <>
-            <div className="m-6">Home View Placeholder Content.</div>
+            <div className="m-6">Home View Placeholder Content. Root Folder ID: {session.data.user.rootFolderId}</div>
         </>
     );
 }
