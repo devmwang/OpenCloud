ffa554468fa3a3ac90bc7d9e4443441b1874a826
Michael Wang <41971562+devmwang@users.noreply.github.com>
2023-08-21 01:44:13 -0700

    fix: Typo in middleware function

--- apps/webui/src/middleware.ts
+++ apps/webui/src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-    const accessToken = request.cookies.get("accessToken");
+    const accessToken = request.cookies.get("AccessToken");
 
     // If there is no access token, do not allow access to protected pages
     if (
