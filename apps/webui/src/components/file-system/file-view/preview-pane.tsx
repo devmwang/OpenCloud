66523b974577814bb43c3c270db15a93109c60ac
Michael Wang <41971562+devmwang@users.noreply.github.com>
2023-08-27 20:23:37 -0700

    Add zoom in/out buttons and current scale display

--- apps/webui/src/components/file-system/file-view/preview-pane.tsx
+++ apps/webui/src/components/file-system/file-view/preview-pane.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react";
 import Image from "next/image";
+import { ZoomIn, ZoomOut } from "lucide-react";
 
 import { env } from "@/env/env.mjs";
 
@@ -10,11 +11,19 @@ export function PreviewPane({ fileId }: { fileId: string }) {
 
     const [scale, setScale] = useState(1);
 
+    function incrementScale() {
+        setScale(Math.min(scale + zoomSpeed, 2));
+    }
+
+    function decrementScale() {
+        setScale(Math.max(scale - zoomSpeed, 0.5));
+    }
+
     function onMouseWheel(event: WheelEvent) {
         if (event.deltaY > 0) {
-            setScale(Math.max(scale - zoomSpeed, 0.5));
+            decrementScale();
         } else {
-            setScale(Math.min(scale + zoomSpeed, 2));
+            incrementScale();
         }
     }
 
@@ -27,7 +36,7 @@ export function PreviewPane({ fileId }: { fileId: string }) {
     return (
         <>
             <Image
-                className="object-scale-down"
+                className="select-none object-scale-down"
                 style={{ transform: `scale(${scale})` }}
                 src={`${env.NEXT_PUBLIC_OPENCLOUD_SERVER_URL}/v1/files/get/${fileId}`}
                 fill={true}
@@ -35,6 +44,21 @@ export function PreviewPane({ fileId }: { fileId: string }) {
                 placeholder="data:image/svg+xml;base64,Cjxzdmcgd2lkdGg9IjcwMCIgaGVpZ2h0PSI0NzUiIHZlcnNpb249IjEuMSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayI+CiAgPGRlZnM+CiAgICA8bGluZWFyR3JhZGllbnQgaWQ9ImciPgogICAgICA8c3RvcCBzdG9wLWNvbG9yPSIjMzMzIiBvZmZzZXQ9IjIwJSIgLz4KICAgICAgPHN0b3Agc3RvcC1jb2xvcj0iIzIyMiIgb2Zmc2V0PSI1MCUiIC8+CiAgICAgIDxzdG9wIHN0b3AtY29sb3I9IiMzMzMiIG9mZnNldD0iNzAlIiAvPgogICAgPC9saW5lYXJHcmFkaWVudD4KICA8L2RlZnM+CiAgPHJlY3Qgd2lkdGg9IjcwMCIgaGVpZ2h0PSI0NzUiIGZpbGw9IiMzMzMiIC8+CiAgPHJlY3QgaWQ9InIiIHdpZHRoPSI3MDAiIGhlaWdodD0iNDc1IiBmaWxsPSJ1cmwoI2cpIiAvPgogIDxhbmltYXRlIHhsaW5rOmhyZWY9IiNyIiBhdHRyaWJ1dGVOYW1lPSJ4IiBmcm9tPSItNzAwIiB0bz0iNzAwIiBkdXI9IjFzIiByZXBlYXRDb3VudD0iaW5kZWZpbml0ZSIgIC8+Cjwvc3ZnPg=="
                 unoptimized={true}
             />
+            <div className="absolute bottom-8 left-1/2 flex -translate-x-1/2 select-none flex-row items-center rounded-full bg-zinc-800/60">
+                <ZoomOut
+                    className="m-4 h-6 cursor-pointer"
+                    onClick={() => {
+                        decrementScale();
+                    }}
+                />
+                <span className="text-lg font-medium">{Math.round(scale * 100)}%</span>
+                <ZoomIn
+                    className="m-4 h-6 cursor-pointer"
+                    onClick={() => {
+                        incrementScale();
+                    }}
+                />
+            </div>
         </>
     );
 }
