10b8e091a186215250c870e12121cecb7b7b449d
Michael Wang <41971562+devmwang@users.noreply.github.com>
2023-08-29 20:53:08 -0700

    Manually set React Suspense (loading state bug work around)

--- apps/webui/src/app/(authenticated)/folder/[folderId]/layout.tsx
+++ apps/webui/src/app/(authenticated)/folder/[folderId]/layout.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { Suspense } from "react";
+
+import Loading from "./loading-page";
 
 export default function FolderLayout(props: { children: React.ReactNode; modal: React.ReactNode }) {
     return (
         <>
-            {props.children}
+            <Suspense fallback={<Loading />}>{props.children}</Suspense>
+
             {props.modal}
         </>
     );
