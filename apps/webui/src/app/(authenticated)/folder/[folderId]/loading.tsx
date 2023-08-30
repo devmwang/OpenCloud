3d5797c011313cf9274ea79f17a8addfdfa73a24
Michael Wang <41971562+devmwang@users.noreply.github.com>
2023-08-29 20:34:39 -0700

    Fill loading page with 8 folder skeletons and 8 file skeletons

--- apps/webui/src/app/(authenticated)/folder/[folderId]/loading.tsx
+++ apps/webui/src/app/(authenticated)/folder/[folderId]/loading.tsx
@@ -25,29 +25,43 @@ export default function Loading() {
 
             <div className="mb-2 text-xl font-medium">Folders</div>
             <div className="mb-6 grid grid-cols-fs-grid-view gap-4">
-                <div className="relative overflow-hidden rounded-xl border border-zinc-200 bg-white shadow-sm before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:border-t before:border-zinc-700/40 before:bg-gradient-to-r before:from-transparent before:via-zinc-700/40 before:to-transparent dark:border-zinc-700 dark:bg-zinc-800">
-                    <div className="flex items-center">
-                        <div className="block px-4 py-3">
-                            <Folder className="h-6" />
+                {Array.from(Array(8).keys()).map((number) => {
+                    return (
+                        <div
+                            key={number}
+                            className="relative overflow-hidden rounded-xl border border-zinc-200 bg-white shadow-sm before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:border-t before:border-zinc-700/40 before:bg-gradient-to-r before:from-transparent before:via-zinc-700/40 before:to-transparent dark:border-zinc-700 dark:bg-zinc-800"
+                        >
+                            <div className="flex items-center">
+                                <div className="block px-4 py-3">
+                                    <Folder className="h-6 text-zinc-900" />
+                                </div>
+                                <div className="mr-2.5 h-6 w-full truncate rounded-lg bg-zinc-900/60"></div>
+                            </div>
                         </div>
-                        <div className="mr-2.5 h-6 w-full truncate rounded-lg bg-zinc-900/60"></div>
-                    </div>
-                </div>
+                    );
+                })}
             </div>
 
             <div className="mb-2 text-xl font-medium">Files</div>
             <div className="mb-6 grid grid-cols-fs-grid-view gap-4">
-                <div className="relative overflow-hidden rounded-xl border border-zinc-200 bg-white shadow-sm before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:border-t before:border-zinc-700/40 before:bg-gradient-to-r before:from-transparent before:via-zinc-700/40 before:to-transparent dark:border-zinc-700 dark:bg-zinc-800">
-                    <div className="flex items-center">
-                        <div className="block px-4 py-3">
-                            <File className="h-6" />
+                {Array.from(Array(8).keys()).map((number) => {
+                    return (
+                        <div
+                            key={number}
+                            className="relative overflow-hidden rounded-xl border border-zinc-200 bg-white shadow-sm before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:border-t before:border-zinc-700/40 before:bg-gradient-to-r before:from-transparent before:via-zinc-700/40 before:to-transparent dark:border-zinc-700 dark:bg-zinc-800"
+                        >
+                            <div className="flex items-center">
+                                <div className="block px-4 py-3">
+                                    <File className="h-6 text-zinc-900" />
+                                </div>
+                                <div className="mr-2.5 h-6 w-full truncate rounded-lg bg-zinc-900/60"></div>
+                            </div>
+                            <div className="px-2.5 pb-2">
+                                <div className="aspect-[3/2] w-full rounded-md bg-zinc-900/60"></div>
+                            </div>
                         </div>
-                        <div className="mr-2.5 h-6 w-full truncate rounded-lg bg-zinc-900/60"></div>
-                    </div>
-                    <div className="px-2.5 pb-2">
-                        <div className="aspect-[3/2] w-full rounded-md bg-zinc-900/60"></div>
-                    </div>
-                </div>
+                    );
+                })}
             </div>
         </div>
     );
