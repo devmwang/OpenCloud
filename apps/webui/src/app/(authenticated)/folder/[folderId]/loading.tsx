d7ddf488e32d6941f93d20708fd9769635168986
Michael Wang <41971562+devmwang@users.noreply.github.com>
2023-08-29 20:21:34 -0700

    Create folders/files animated loading skeleton

--- apps/webui/src/app/(authenticated)/folder/[folderId]/loading.tsx
+++ apps/webui/src/app/(authenticated)/folder/[folderId]/loading.tsx
@@ -1,4 +1,4 @@
-import { ChevronRight } from "lucide-react";
+import { ChevronRight, Folder, File } from "lucide-react";
 
 export default function Loading() {
     return (
@@ -13,17 +13,42 @@ export default function Loading() {
                     </div>
 
                     <div className="flex items-center">
-                        <div className="relative ml-2.5 mr-3 h-10 w-40 cursor-default overflow-hidden rounded-xl bg-zinc-900 pb-1 pt-0.5 before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:border-t before:border-zinc-700/30 before:bg-gradient-to-r before:from-transparent before:via-zinc-700/30 before:to-transparent"></div>
+                        <div className="relative ml-2.5 mr-3 h-10 w-40 cursor-default overflow-hidden rounded-xl bg-zinc-900 pb-1 pt-0.5 before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:border-t before:border-zinc-700/40 before:bg-gradient-to-r before:from-transparent before:via-zinc-700/40 before:to-transparent"></div>
                         <div className="block">
                             <ChevronRight className="mt-0.5 h-6" />
                         </div>
                     </div>
 
-                    <div className="relative ml-2.5 mr-3 h-10 w-40 cursor-default overflow-hidden rounded-xl bg-zinc-900 pb-1 pt-0.5 before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:border-t before:border-zinc-700/30 before:bg-gradient-to-r before:from-transparent before:via-zinc-700/30 before:to-transparent"></div>
+                    <div className="relative ml-2.5 mr-3 h-10 w-40 cursor-default overflow-hidden rounded-xl bg-zinc-900 pb-1 pt-0.5 before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:border-t before:border-zinc-700/40 before:bg-gradient-to-r before:from-transparent before:via-zinc-700/40 before:to-transparent"></div>
                 </div>
             </div>
 
-            {/* <GridLayout folders={folderContents.data.folders} files={folderContents.data.files} /> */}
+            <div className="mb-2 text-xl font-medium">Folders</div>
+            <div className="mb-6 grid grid-cols-fs-grid-view gap-4">
+                <div className="relative overflow-hidden rounded-xl border border-zinc-200 bg-white shadow-sm before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:border-t before:border-zinc-700/40 before:bg-gradient-to-r before:from-transparent before:via-zinc-700/40 before:to-transparent dark:border-zinc-700 dark:bg-zinc-800">
+                    <div className="flex items-center">
+                        <div className="block px-4 py-3">
+                            <Folder className="h-6" />
+                        </div>
+                        <div className="mr-2.5 h-6 w-full truncate rounded-lg bg-zinc-900/60"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="mb-2 text-xl font-medium">Files</div>
+            <div className="mb-6 grid grid-cols-fs-grid-view gap-4">
+                <div className="relative overflow-hidden rounded-xl border border-zinc-200 bg-white shadow-sm before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:border-t before:border-zinc-700/40 before:bg-gradient-to-r before:from-transparent before:via-zinc-700/40 before:to-transparent dark:border-zinc-700 dark:bg-zinc-800">
+                    <div className="flex items-center">
+                        <div className="block px-4 py-3">
+                            <File className="h-6" />
+                        </div>
+                        <div className="mr-2.5 h-6 w-full truncate rounded-lg bg-zinc-900/60"></div>
+                    </div>
+                    <div className="px-2.5 pb-2">
+                        <div className="aspect-[3/2] w-full rounded-md bg-zinc-900/60"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
