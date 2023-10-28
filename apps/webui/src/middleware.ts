0af1f9eeabbf4fad0ddae7f05cba766661937b45
Michael Wang <41971562+devmwang@users.noreply.github.com>
2023-10-27 19:08:12 -0700

    fix: Discord user agent

--- apps/webui/src/middleware.ts
+++ apps/webui/src/middleware.ts
@@ -19,7 +19,7 @@ export async function middleware(request: NextRequest) {
     }
 
     // Handle discordbot user agent
-    if (request.headers.get("User-Agent")?.toLowerCase().includes("discord")) {
+    if (request.headers.get("User-Agent")?.toLowerCase().includes("discord") || request.headers.get("User-Agent") == "Mozilla/5.0 (Macintosh; Intel Mac OS X 11.6; rv:92.0) Gecko/20100101 Firefox/92.0") {
         const pathName = request.nextUrl.pathname;
         const fileId = pathName.substring(pathName.lastIndexOf('/') + 1);
 
