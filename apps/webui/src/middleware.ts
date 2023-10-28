24e50eb999f5a15362914d7eeba4d1eecc3e981d
Michael Wang <41971562+devmwang@users.noreply.github.com>
2023-10-28 16:47:08 -0700

    fix: Fix edge-case in middleware for discord user agent detection

--- apps/webui/src/middleware.ts
+++ apps/webui/src/middleware.ts
@@ -19,10 +19,12 @@ export async function middleware(request: NextRequest) {
     }
 
     // Handle discordbot user agent
-    if (request.headers.get("User-Agent")?.toLowerCase().includes("discord") || request.headers.get("User-Agent") == "Mozilla/5.0 (Macintosh; Intel Mac OS X 11.6; rv:92.0) Gecko/20100101 Firefox/92.0") {
-        const pathName = request.nextUrl.pathname;
-        const fileId = pathName.substring(pathName.lastIndexOf('/') + 1);
-
-        return NextResponse.rewrite(new URL(`${env.NEXT_PUBLIC_OPENCLOUD_SERVER_URL}/v1/files/get/${fileId}`));
+    if (request.nextUrl.pathname.startsWith("/file")) {
+        if (request.headers.get("User-Agent")?.toLowerCase().includes("discord") || request.headers.get("User-Agent") == "Mozilla/5.0 (Macintosh; Intel Mac OS X 11.6; rv:92.0) Gecko/20100101 Firefox/92.0") {
+            const pathName = request.nextUrl.pathname;
+            const fileId = pathName.substring(pathName.lastIndexOf('/') + 1);
+    
+            return NextResponse.rewrite(new URL(`${env.NEXT_PUBLIC_OPENCLOUD_SERVER_URL}/v1/files/get/${fileId}`));
+        }
     }
 }
